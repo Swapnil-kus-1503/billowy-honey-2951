05f80b76961aeefd9fe57be46e095230be99feca
Swapnil-kus-1503 <ashukushwaha15031997@gmail.com>
2022-07-22 09:06:09 +0530

    day-3

--- billowy_2951/src/Pages/Home.jsx
+++ billowy_2951/src/Pages/Home.jsx
@@ -1,49 +1,62 @@
+import { Box, Button, Container, Flex, Image, Text } from "@chakra-ui/react";
 import React from "react";
 import Footer from "../Components/Footer";
+import styles from "../Styles/Home.module.css";
 
 const Home = () => {
   return (
     <div>
-      <img src="https://cloudfront.fitnessblender.com/assets/img/homepage/team-720.webp" alt="" />
-      <div class="unit-block full passes">
-        <div class="unit-content mc-dc">
-          <h2 class="unit-title">
-            Introducing<br/>
-            <span class="fbplus">
-              <span class="fb">FB</span>
-              <span class="plus">Plus</span>
-            </span>
-            Passes
-          </h2>
-          <p class="unit-body">Passes give you full access to everything FB Plus
-            has to offer, without a long-term subscription. Choose a pass that
-            fits your budget and your schedule.</p>
-          <div class="unit-cta">
-            <a class="btn -dark" href="https://www.fitnessblender.com/store#store-passes">Buy a Pass</a>
-          </div>
-        </div>
-        <div class="unit-content wide mc-dc">
-          <picture class="unit-image">
-          <source type="image/webp" data-srcset="
-                      https://cloudfront.fitnessblender.com/assets/img/homepage/passes-1440.webp 1440w,
-            https://cloudfront.fitnessblender.com/assets/img/homepage/passes-720.webp 720w
-          " sizes="(max-width: 940px) 100vw, 50vw" srcset="
-                      https://cloudfront.fitnessblender.com/assets/img/homepage/passes-1440.webp 1440w,
-            https://cloudfront.fitnessblender.com/assets/img/homepage/passes-720.webp 720w
-          ">
-      
-          <source type="image/png" data-srcset="
-                      https://cloudfront.fitnessblender.com/assets/img/homepage/passes-1440.png 1440w,
-            https://cloudfront.fitnessblender.com/assets/img/homepage/passes-720.png 720w
-          " sizes="(max-width: 940px) 100vw, 50vw" srcset="
-                      https://cloudfront.fitnessblender.com/assets/img/homepage/passes-1440.png 1440w,
-            https://cloudfront.fitnessblender.com/assets/img/homepage/passes-720.png 720w
-          ">
-        <img class="lazyfade ls-is-cached lazyloaded" data-src="https://cloudfront.fitnessblender.com/assets/img/homepage/passes-1440.png" alt="FB Plus passes" width="720" height="250" src="https://cloudfront.fitnessblender.com/assets/img/homepage/passes-1440.png">
-  </picture>
-        </div>
+      <div className={styles.container}>
+        <Flex gap="2rem">
+          <Box className={styles.box1}>
+            <h1 className={styles.h1}>
+              Feel Great.
+              <br />
+              Body And Mind.
+            </h1>
+
+            <h2 className={styles.text} style={{ alignContent: "center" }}>
+              Choose from hundreds of workouts, healthy recipes, relaxing
+              meditations, and expert articles, for a whole body and mind
+              approach to feeling great.
+            </h2>
+            <Box>
+              <Button
+                className={styles.joinNow}
+                size="lg"
+                background={"#212432"}
+                textTransform="uppercase"
+              >
+                Join Now
+              </Button>
+            </Box>
+          </Box>
+          <Box className={styles.box2}>
+            <Image
+              width="700px"
+              src="https://cloudfront.fitnessblender.com/assets/img/homepage/team-720.webp"
+              alt="home"
+            />
+          </Box>
+        </Flex>
       </div>
-      <Footer/>
+
+      <div className={styles.container2}>
+        <Box className={styles.container2h1}>
+          <h1 style={{ marginLeft: "35rem" }}>
+            Introducing
+            <br />
+            FB <span color="">Plus</span> Passes.
+          </h1>
+          <Text>
+            Passes give you full access to everything FB Plus has to offer,
+            without a long-term subscription. Choose a pass that fits your
+            budget and your schedule.
+          </Text>
+        </Box>
+      </div>
+
+      <Footer />
     </div>
   );
 };
